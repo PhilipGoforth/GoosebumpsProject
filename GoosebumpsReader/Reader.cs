02e5b8bc45f186f33432e46e6feeaec14123b0b1
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-11-10 11:18:18 -0600

    added end menu

--- GoosebumpsReader/Reader.cs
+++ GoosebumpsReader/Reader.cs
@@ -31,10 +31,36 @@ namespace GoosebumpsReader
             switch (selectedIndex)
             {
                 case 0:
-                    Game.RunChoice();
+                    MainStory.RunChoice();
                     break;
                 case 1:
-                    Game.ExitGame();
+                    MainStory.ExitGame();
+                    break;
+            }
+        }
+        public static void TheEnd()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            string prompt = @"
+                           ▄▄▄▄▀ ▄  █ ▄███▄       ▄███▄      ▄   ██▄   
+                        ▀▀▀ █   █   █ █▀   ▀      █▀   ▀      █  █  █  
+                            █   ██▀▀█ ██▄▄        ██▄▄    ██   █ █   █ 
+                           █    █   █ █▄   ▄▀     █▄   ▄▀ █ █  █ █  █  
+                          ▀        █  ▀███▀       ▀███▀   █  █ █ ███▀  
+                                       ▀                       █         
+                                               
+                                     What would you like to do?
+                               (Use arrow keys and Enter to select.)";
+            string[] options = { "Play Again", "Exit" };
+            Menu EndMenu = new Menu(prompt, options);
+            int selectedIndex = EndMenu.Run();
+            switch (selectedIndex)
+            {
+                case 0:
+                    MainStory.RunChoice();
+                    break;
+                case 1:
+                    MainStory.ExitGame();
                     break;
             }
         }
